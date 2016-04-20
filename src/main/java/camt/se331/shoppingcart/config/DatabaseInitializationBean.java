be0f8935d58d2e6dad9912643af372a17692c57d
skyonsky <pranpoom_dis@cmu.ac.th>
2016-04-20 22:38:51 +0700

    2.7 cannot login

--- src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
+++ src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
@@ -1,27 +1,27 @@
 package camt.se331.shoppingcart.config;
 
 import camt.se331.shoppingcart.dao.ShoppingCartDao;
-import camt.se331.shoppingcart.entity.Product;
-import camt.se331.shoppingcart.entity.SelectedProduct;
-import camt.se331.shoppingcart.entity.ShoppingCart;
+import camt.se331.shoppingcart.entity.*;
 import camt.se331.shoppingcart.repository.ProductRepository;
 import camt.se331.shoppingcart.repository.ShoppingCartRepository;
+import camt.se331.shoppingcart.repository.UserRepository;
 import camt.se331.shoppingcart.service.ImageUtil;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.*;
 
 /**
  * Created by Dto on 2/11/2015.
  */
-@CrossOrigin
 @Component
 @Profile("db.init")
 public class DatabaseInitializationBean implements InitializingBean {
+    @Autowired
+    UserRepository userRepository;
+
     @Autowired
     ProductRepository productRepository;
     @Autowired
@@ -56,5 +56,31 @@ public class DatabaseInitializationBean implements InitializingBean {
         shoppingCart.setPurchaseDate(calendar.getTime());
         shoppingCart.setId(1L);
         shoppingCartRepository.save(shoppingCart);
+
+
+        Role adminRole = new Role("admin");
+        Role userRole = new Role("user");
+
+        User admin = new User();
+        admin.setName("admin");
+        admin.setUsername("admin");
+        admin.setEmail("admin@yahoo.com");
+        admin.setPassword("123456");
+        Set<Role> roles = new HashSet<>();
+        roles.add(adminRole);
+        admin.setRoles(roles);
+
+        User user = new User();
+        user.setName("user");
+        user.setUsername("user");
+        user.setEmail("user@yahoo.com");
+        user.setPassword("123456");
+        Set<Role> roles2 = new HashSet<>();
+        roles2.add(userRole);
+        user.setRoles(roles2);
+        userRepository.save(admin);
+        userRepository.save(user);
+        admin.setRoles(roles);
+        user.setRoles(roles2);
     }
 }
